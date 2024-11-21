191ed7d698cf5986f315cc50d8632b9e9bdfb908
Long Nguyen <zedsc03@gmail.com>
2024-11-21 16:07:16 +0700

    Update Spark Operator

--- plugins/Operators/SparkProcessOperator.py
+++ plugins/Operators/SparkProcessOperator.py
@@ -8,16 +8,7 @@ import pyspark.sql.types as T
 from pyspark.sql import SparkSession
 from pyspark.sql import DataFrame
 
-# from airflow.models.baseoperator import BaseOperator
-# from airflow.utils.decorators import apply_defaults
-from pyspark.sql.types import (
-    StructType, 
-    StructField, 
-    TimestampType,
-    FloatType,
-    IntegerType,
-    StringType
-)
+from airflow.models.baseoperator import BaseOperator
 
 
 
@@ -39,7 +30,7 @@ kafka_output_config = {
 }
 
 
-class SparkProcessOperator:
+class SparkProcessOperator(BaseOperator):
     """ 
     Defines target class SpotifyApiOperator in conjunction 
     with Spotify API, inheriting from class BaseOperator.
@@ -51,9 +42,8 @@ class SparkProcessOperator:
     :param secure
     """
     
-    # @apply_defaults
-    def __init__(self, config: dict, appName: str = None, *args, **kwargs) -> None:
-        # super().__init__(*args, **kwargs)
+    def __init__(self, config: dict, appName: str, **kwargs: str) -> None:
+        super().__init__(**kwargs)
         self.appName = appName
         self._config = config
         
@@ -65,12 +55,13 @@ class SparkProcessOperator:
             if self._config is not None:
                 s_conn: SparkSession = SparkSession.builder \
                         .appName("{}-{}".format(self.appName, datetime.today())) \
-                        .master("spark://172.19.0.3:7077") \
+                        .master("spark://172.19.0.5:7077") \
                         .config("spark.hadoop.fs.s3a.endpoint", "http://" + self._config["endpoint_url"]) \
                         .config("spark.hadoop.fs.s3a.access.key", str(self._config["aws_access_key_id"])) \
                         .config("spark.hadoop.fs.s3a.secret.key", str(self._config["aws_secret_access_key"])) \
                         .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
                         .config("spark.hadoop.fs.s3a.path.style.access", "true") \
+                        .config("spark.jars", "/opt/bitnami/spark/jars/spark-sql-kafka-0-10_2.13-3.5.3.jar") \
                         .getOrCreate()
                         
                 s_conn.sparkContext.setLogLevel("ERROR")
@@ -90,12 +81,12 @@ class SparkProcessOperator:
             if spark is not None:
                 if schema is not None:
                     streaming_df = spark.readStream \
-                                    .format("kafka") \
-                                    .option("kafka.bootstrap.servers", "kafka:9092") \
-                                    .option("subscribe", topic_name) \
-                                    .option("startingOffsets", "earliest").load() \
-                                    .select(F.from_json(F.col("value").cast("string"), schema).alias("json_data")) \
-                                    .select("json_data.*")                                
+                        .format("kafka") \
+                        .option("kafka.bootstrap.servers", "kafka:9092") \
+                        .option("subscribe", topic_name) \
+                        .option("startingOffsets", "earliest").load() \
+                        .select(F.from_json(F.col("value").cast("string"), schema).alias("json_data")) \
+                        .select("json_data.*")                                
                                     
                     logger.info("kafka dataframe created successfully")
                     return streaming_df
@@ -106,39 +97,3 @@ class SparkProcessOperator:
             
     def streaming_OHLCV_data_process(self) -> DataFrame:
         pass
-        
-        
-        
-if __name__ == "__main__":
-    schema = StructType([
-        StructField("Datetime", TimestampType(), True),
-        StructField("Open", FloatType(), True),
-        StructField("High", FloatType(), True),
-        StructField("Low", FloatType(), True),
-        StructField("Close", FloatType(), True),
-        StructField("Adj Close", FloatType(), True),
-        StructField("Volume", IntegerType(), True),
-        StructField("Dividends", FloatType(), True),
-        StructField("Stock Splits", FloatType(), True),
-        StructField("ticker", StringType(), True)
-    ])
-    
-    spark_stream = SparkProcessOperator(
-        # task_id="test_stream",
-        config={
-            "endpoint_url": "",
-            "aws_access_key_id": "",
-            "aws_secret_access_key": ""
-        }, appName="Streaming ingestion data"
-    )
-    
-    spark_df = spark_stream.connect_to_kafka(schema, "yfinance_stock")
-    if spark_df is not None:
-        logging.info("Streaming is being started...")
-        
-        streaming_query = (spark_df.writeStream.format("csv")
-                               .option('path', './data')
-                               .option('checkpointLocation', './checkpoints')
-                               .start())
-
-        streaming_query.awaitTermination()
