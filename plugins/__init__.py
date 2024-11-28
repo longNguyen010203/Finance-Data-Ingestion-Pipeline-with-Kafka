fcc9434013bba6e918207432fed60bd5dc43283d
Long Nguyen <zedsc03@gmail.com>
2024-11-28 15:20:35 +0700

    Add

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,8 +1,18 @@
 from airflow.plugins_manager import AirflowPlugin
 from Operators.YahooFinanceOperator import YahooFinanceOperator
+from Operators.MinioReaderWriterOperator import MinioReaderWriterOperator
+from Operators.KafkaConsumeOperator import KafkaConsumeOperator
+from Operators.SparkProcessOperator import SparkProcessOperator
 
 
 
-class YahooFinanceOperator(AirflowPlugin):
-    name = "YahooFinance_API_Plugin"
-    operators = [YahooFinanceOperator]
+
+
+class CustomOperatorsPlugin(AirflowPlugin):
+    name = "custom_operators_plugin"
+    operators = [
+        YahooFinanceOperator,
+        MinioReaderWriterOperator,
+        KafkaConsumeOperator,
+        SparkProcessOperator
+    ]
